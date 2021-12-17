7dc2f0c345340b045c129d20ed9ea5c7580180a1
Daniel Alexander Muelas Rivera <desarrollador@seguridadomega.com.co>
2021-12-17 16:55:55 -0500

    * Se leen las opciones del menu del fichero json

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_utils.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,21 +17,48 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    menuProvider.cargarData();
+    // menuProvider.cargarData();
+    // return ListView(
+    //   children: _listaItems(),
+    // );
 
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      // initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        return ListView(
+          children: _listaItems(snapshot.data, context),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider(),
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider(),
-      const ListTile(title: Text('Hola Mundo')),
-      const Divider()
-    ];
+  List<Widget> _listaItems(List<dynamic>? data, BuildContext context) {
+    final List<Widget> opciones = [];
+
+    data?.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: const Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.blue,
+        ),
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context) => const AlertPage(),
+          );
+          Navigator.push(context, route);
+        },
+      );
+
+      opciones
+        ..add(widgetTemp)
+        ..add(const Divider());
+    });
+
+    return opciones;
   }
 }
