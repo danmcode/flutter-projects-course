3849c195f7830723be01d22274e868501cb23693
Daniel Alexander Muelas Rivera <desarrollador@seguridadomega.com.co>
2021-12-20 17:14:07 -0500

    Nueva aplicacion de componentes

---
+++ fl_components/lib/screens/home_screen.dart
@@ -0,0 +1,40 @@
+import 'package:fl_components/models/models.dart';
+import 'package:fl_components/router/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final menuOptions = AppRoutes.menuOptions;
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+          itemBuilder: (context, index) => ListTile(
+            leading: Icon(
+              menuOptions[index].icon,
+              color: Colors.indigoAccent,
+            ),
+            title: Text(menuOptions[index].name),
+            onTap: () {
+              //Forma 1
+              // final route = MaterialPageRoute(
+              //   builder: (context) => const ListView1Screen(),
+              // );
+
+              Navigator.pushNamed(
+                context,
+                menuOptions[index].route,
+              );
+            },
+          ),
+          separatorBuilder: (_, __) => const Divider(),
+          itemCount: menuOptions.length,
+        ));
+  }
+}
